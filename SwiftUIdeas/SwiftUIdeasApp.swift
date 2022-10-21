e238ba422b01cec2f7cc4cc1494dcdac422c4d9b
JorgeAcostaFreire <jorge.acosta.freire@gmail.com>
2022-10-21 16:34:32 +0200

    Initial Commit

---
+++ SwiftUIdeas/SwiftUIdeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIdeasApp.swift
+//  SwiftUIdeas
+//
+//  Created by Jorge Acosta Freire on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
